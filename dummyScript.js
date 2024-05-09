15f458ca3f31927604fef901b1093717c56ede86
kmaasilan <146767770+kmaasilan@users.noreply.github.com>
2024-05-08 20:09:28 -0700

    Update dummyScript.js to be more readable

--- dummyScript.js
+++ dummyScript.js
@@ -1,5 +1,7 @@
 export function factorial (value) {
-    if(value === 0 || value == 1)
+    if(value === 0)
+        return 1;
+    else if(value == 1)
         return 1;
     return value * factorial(value - 1);
 }
