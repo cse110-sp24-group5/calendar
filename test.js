417abd4f34d1cb7cdb8b6e252981caf480ddbe14
Drishti Regmi <110417319+dregmi08@users.noreply.github.com>
2024-05-10 21:17:04 -0700

    Update test.js
    Added javadoc comments to test.js

--- test.js
+++ test.js
@@ -1,20 +1,24 @@
+/** Import the function that we want to test*/
 import {
     factorial
 } from './dummyScript.js';
 
-
+/** This tests if the factorial function outputs 1 if it is passed param 0*/
 test('factorial 0', () => {
     expect(factorial(0)).toBe(1);
 });
 
+/** This tests if the factorial function outputs 1 if it is passed param 1*/
 test('factorial 1', () => {
     expect(factorial(1)).toBe(1);
 });
 
+/** This tests if the factorial function outputs 120 if it is passed param 5*/
 test('factorial 5', () => {
     expect(factorial(5)).toBe(120);
 });
 
+/** This tests if the factorial function outputs 720 if it is passed param 9*/
 test('factorial 9', () => {
     expect(factorial(6)).toBe(720);
 });
