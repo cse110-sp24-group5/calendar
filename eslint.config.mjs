0d5578e5e873e3b18b26009a923b3e1509892ea2
kmaasilan <146767770+kmaasilan@users.noreply.github.com>
2024-05-11 00:34:22 -0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,21 +2,12 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginJest from "eslint-plugin-jest";
 
-export default {
-  ...pluginJs.configs.recommended,
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      jest: "readonly",
-      // Add any other global variables here if needed
-    },
+
+export default [
+  {
+    plugins: { jest: pluginJest }, 
+    languageOptions: { jest: "readonly" }
   },
-  plugins: {
-    ...pluginJs.plugins,
-    jest: pluginJest,
-  },
-  env: {
-    ...pluginJs.env,
-    jest: true, // Set Jest environment
-  },
-};
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended
+];
