2a3c2f334a41c4f33ea973971d2ba438952dccd9
kmaasilan <146767770+kmaasilan@users.noreply.github.com>
2024-05-10 19:47:23 -0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,6 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default {
-  // Set global variables for browser environment
-  globals: globals.browser,
-
   // Extend with recommended configuration from pluginJs
   extends: [pluginJs.configs.recommended],
 
