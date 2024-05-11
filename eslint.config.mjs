dad8afc087f22d0213986de76e0e09ab9d110f11
kmaasilan <146767770+kmaasilan@users.noreply.github.com>
2024-05-10 20:46:41 -0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,16 @@
 import pluginJs from "@eslint/js";
 import pluginJest from "eslint-plugin-jest";
 
-export default [
-  {
-    files: ["*.js"], // Adjust the pattern to match your project's JavaScript files
-    rules: {
-      // Define ESLint rules directly here if needed
-      // Example:
-      // "no-console": "off"
-    },
-    plugins: ["jest"], // Add Jest plugin
-    env: {
-      jest: true, // Set Jest environment
-    },
+export default {
+  plugins: {
+    jest: pluginJest,
   },
-];
+  rules: {
+    // Define ESLint rules here if needed
+    // Example:
+    // "no-console": "off"
+  },
+  env: {
+    jest: true, // Set Jest environment
+  },
+};
