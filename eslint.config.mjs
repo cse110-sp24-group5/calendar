98326772c3090f7871ee85aca4485bdc9ec85014
kmaasilan <146767770+kmaasilan@users.noreply.github.com>
2024-05-11 00:55:34 -0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,6 @@ import pluginJest from "eslint-plugin-jest";
 
 export default [
   {plugins: { jest: pluginJest }},
-  languageOptions: { globals: { globals.browser, pluginJest.jest }},
+  {languageOptions: { globals: { globals.browser, pluginJest.jest }}},
   pluginJs.configs.recommended
 ];
