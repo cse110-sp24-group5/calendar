1cc96fdde9e435e1a43a2e7c169a99004fd3b86c
mishkajethwani <mjethwani@ucsd.edu>
2024-05-12 11:17:32 -0700

    fixing unit tests

---
+++ test.js
@@ -0,0 +1,20 @@
+import {
+    factorial
+} from '../dummyScript.js';
+
+
+test('factorial 0', () => {
+    expect(factorial(0)).toBe(1);
+});
+
+test('factorial 1', () => {
+    expect(factorial(1)).toBe(1);
+});
+
+test('factorial 5', () => {
+    expect(factorial(5)).toBe(120);
+});
+
+test('factorial 9', () => {
+    expect(factorial(6)).toBe(720);
+});
