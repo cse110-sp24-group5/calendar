8a0aac7c6d829efb417accf23347108fd5c6f56b
mishkajethwani <122850345+mishkajethwani@users.noreply.github.com>
2024-05-12 10:41:48 -0700

    Update tests.e2e.js

---
+++ __tests__/tests.e2e.js
@@ -0,0 +1,21 @@
+describe('Basic user flow for Website', () => {
+    beforeAll(async () => {
+        await page.goto('https://cse110-sp24-group5.github.io/calendar/');
+    });
+
+    it('Test', async () => {
+        const calendarWidgetParent = await page.$('.calendar-widget.parent');
+        const calendarChild = await calendarWidgetParent.$('.calendar.child');
+        const days = await calendarChild.$('.days');
+        const day0 = await days.$('li');
+        await day0.click();
+        const newTaskInput = await page.$('#newTaskInput');
+        await newTaskInput.type('temp');
+        const addTaskButton = await page.$('#addTaskButton');
+        await addTaskButton.click();
+        const taskBox = await page.$('.task-box');
+        const taskListUl = await taskBox.$('.task-list-ul');
+        const lis = await taskListUl.$$('li');
+        expect(lis.length).toBe(1);
+    });
+});
