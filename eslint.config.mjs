47e3f558917b14bba9843a548e229d5cd05250ec
Arnav710 <mailto.arnav.modi@gmail.com>
2024-05-12 12:42:29 -0700

    fix merge conflicts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,5 @@ import pluginJs from "@eslint/js";
 
 export default [
   {languageOptions: { globals: { ...globals.browser, ...globals.jest, page: "readonly" }}},
-<<<<<<< HEAD
-  pluginJs.configs.recommended,	  pluginJs.configs.recommended,
-=======
   pluginJs.configs.recommended,
->>>>>>> main
 ];
